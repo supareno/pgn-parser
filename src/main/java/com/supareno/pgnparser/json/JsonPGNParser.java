34cd5f56b08d7a4233878731ff14c9489651289c
supareno <supareno@gmail.com>
2014-03-13 08:11:35 +0100

    removing jackson use
    must be reimplmented with json-smart JSON objects

--- src/main/java/com/supareno/pgnparser/json/JsonPGNParser.java
+++ src/main/java/com/supareno/pgnparser/json/JsonPGNParser.java
@@ -17,13 +17,8 @@
  */
 package com.supareno.pgnparser.json;
 
-import java.io.IOException;
 import java.io.Reader;
 
-import org.codehaus.jackson.JsonFactory;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonToken;
 
 import com.supareno.pgnparser.AbstractPGNParser;
 import com.supareno.pgnparser.PGNType;
@@ -40,69 +35,9 @@ public class JsonPGNParser extends AbstractPGNParser {
 
 	@Override
 	public Games parseFile ( Reader reader ) {
+		// TODO: must be reimplmented with json-smart JSON objects
 		Games games = null;
-		// TODO :-)
-		JsonFactory factory = new JsonFactory ();
-		try {
-			JsonParser parser = factory.createJsonParser ( reader );
-			while ( !parser.isClosed () ) {
-				// get the token
-				JsonToken token = parser.nextToken ();
-				// if its the last token then we are done
-				if ( token == null ) {
-					break;
-				}
-				// we want to look for a field that says dataset
-				System.out.println ( token.name () );
-				/*
-				 * "Event": "chp",
-				 * "Site": "USA",
-				 * "Date": "1956.??.??",
-				 * "Round": "?",
-				 * "White": "Byrne, D.",
-				 * "Black": "Fischer, R.",
-				 * "Result": "0-1",
-				 * "hits":
-				 */
-				if ( JsonToken.FIELD_NAME.equals ( token ) && "hits".equals ( parser.getCurrentName () ) ) {
-					// we are entering the datasets now. The first token should be
-					// start of array
-					token = parser.nextToken ();
-					if ( !JsonToken.START_ARRAY.equals ( token ) ) {
-						// bail out
-						break;
-					}
-					// each element of the array is an album so the next token
-					// should be {
-					token = parser.nextToken ();
-					if ( !JsonToken.START_OBJECT.equals ( token ) ) {
-						break;
-					}
-					// we are now looking for a field that says "album_title". We
-					// continue looking till we find all such fields. This is
-					// probably not a best way to parse this json, but this will
-					// suffice for this example.
-					while ( true ) {
-						token = parser.nextToken ();
-						if ( token == null ) {
-							break;
-						}
-						if ( JsonToken.FIELD_NAME.equals ( token ) && "hit".equals ( parser.getCurrentName () ) ) {
-							token = parser.nextToken ();
-							// System.out.println ( parser.getText () );
-						}
-
-					}
-				}
-			}
-
-		} catch ( JsonParseException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace ();
-		} catch ( IOException e ) {
-			// TODO Auto-generated catch block
-			e.printStackTrace ();
-		}
+	
 		return games;
 	}
 
